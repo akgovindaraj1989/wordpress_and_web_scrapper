9cf163fd89cc2f8e955b90be23bc9572f5b82b62
akgovindaraj1989 <139285706+akgovindaraj1989@users.noreply.github.com>
2024-06-24 22:42:57 +0100

    Update article_reader_python.py

--- article_reader/article_reader_python.py
+++ article_reader/article_reader_python.py
@@ -3,7 +3,9 @@ import requests
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
+    # Defining the class MyHTMLParser attibutes
     def __init__(self):
+        # Calling HTMLParser
         super().__init__()
         self.in_titleline = False
         self.in_link = False
@@ -11,25 +13,31 @@ class MyHTMLParser(HTMLParser):
         self.current_link = ""
         self.current_title = ""
 
+    # handle_starttag is the HTMLParser method to redefine in MyHTMLParser 
     def handle_starttag(self, tag, attrs):
         attrs_dict = dict(attrs)
-        if tag == "span" and attrs_dict.get("class") == "titleline":
+        #Checking the html tag with span and class where the article stories starting
+        if tag == "span" and attrs_dict.get("class") == "titleline": 
             self.in_titleline = True
+        # Getting the article link    
         if tag == "a" and self.in_titleline:
             self.current_link = attrs_dict.get("href", "")
             self.in_link = True
-            
+
+    # handle_data is the HTMLParser method to redefine in MyHTMLParser
     def handle_data(self, data):
+        # Getting the article data actual text
         if self.in_link:
             self.current_title = data
 
+    # handle_endtag is the HTMLParser method to redefine in MyHTMLParser
     def handle_endtag(self, tag):
         if tag == "span" and self.in_titleline:
             self.in_titleline = False
         if tag == "a" and self.in_link:
             self.in_link = False
             self.in_titleline = False
-
+       # Appending it to the top_articles list to return to the object.
         if self.current_title and self.current_link:
                 self.top_articles.append((self.current_title, self.current_link))                
                 self.current_title = ""
@@ -56,7 +64,7 @@ def fetch_hacker_news_top_articles():
         return None
 
 if __name__ == "__main__":
-            
+    # call the above defined function fetch_hacker_new_top_articles()        
     top_articles = fetch_hacker_news_top_articles()
 
     if top_articles:        
